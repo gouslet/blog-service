8bab3bcd6f4c9f5e92319e0f961a67998c1d36e9
Elon <13120612730@163.com>
2022-05-30 20:16:51 +0800

    docs: add swagger api for v1.Tag.List

--- internals/routers/api/v1/tag.go
+++ internals/routers/api/v1/tag.go
@@ -4,7 +4,7 @@
  * Created At: Sunday, 2022/05/29 , 00:40:25                                   *
  * Author: elchn                                                               *
  * -----                                                                       *
- * Last Modified: Monday, 2022/05/30 , 17:42:17                                *
+ * Last Modified: Monday, 2022/05/30 , 20:15:10                                *
  * Modified By: elchn                                                          *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -30,6 +30,15 @@ func (t Tag) Get(c *gin.Context) {
 	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
 }
 
+// @Summary get multiple tags
+// @Produce json
+// @Param name query string false "tag name" maxlength(100)
+// @Param state query int false "state" Enum(0, 1) default(1)
+// @Param page query int false "page index"
+// @Param page_size query int false "size per page"
+// @Success 200 {object} model.Tag "Succeeded"
+// @Failure 400 {object} errcode.Error "request errors"
+// @Failure 500 {object} errcode.Error "internal errors"
 func (t Tag) List(c *gin.Context)   {}
 func (t Tag) Create(c *gin.Context) {}
 func (t Tag) Update(c *gin.Context) {}
