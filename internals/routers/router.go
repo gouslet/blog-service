151f40431979bfcca8025b3b1c0e905433a167b3
Elon <13120612730@163.com>
2022-05-30 21:27:18 +0800

    feat: add parameters validation for requests

--- internals/routers/router.go
+++ internals/routers/router.go
@@ -4,7 +4,7 @@
  * Created At: Sunday, 2022/05/29 , 00:36:09                                   *
  * Author: elchn                                                               *
  * -----                                                                       *
- * Last Modified: Monday, 2022/05/30 , 19:41:15                                *
+ * Last Modified: Monday, 2022/05/30 , 21:14:44                                *
  * Modified By: elchn                                                          *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -15,6 +15,7 @@ package routers
 
 import (
 	_ "go_start/blog_service/docs"
+	"go_start/blog_service/internals/middleware"
 	v1 "go_start/blog_service/internals/routers/api/v1"
 
 	"github.com/gin-gonic/gin"
@@ -26,6 +27,7 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
 
 	tag := v1.NewTag()
 	apiV1 := r.Group("/api/v1")
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -4,7 +4,7 @@
  * Created At: Monday, 2022/05/30 , 17:37:21                                   *
  * Author: elchn                                                               *
  * -----                                                                       *
- * Last Modified: Monday, 2022/05/30 , 17:46:31                                *
+ * Last Modified: Monday, 2022/05/30 , 21:26:01                                *
  * Modified By: elchn                                                          *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -15,6 +15,7 @@ package app
 
 import (
 	"go_start/blog_service/pkg/errcode"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,6 +26,14 @@ type Response struct {
 
 func NewResponse(ctx *gin.Context) *Response {
 	return &Response{Ctx: ctx}
+}
+
+func (r *Response) ToResponse(data any) {
+	if data == nil {
+		data = gin.H{}
+	}
+
+	r.Ctx.JSON(http.StatusOK, data)
 }
 
 func (r *Response) ToErrorResponse(err *errcode.Error) {
