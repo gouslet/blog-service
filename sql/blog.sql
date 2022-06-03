65b78aa47c82812f223ac15d7e4cea56dada6f97
Elon <13120612730@163.com>
2022-06-03 13:19:32 +0800

    ci: merge sql files into one file

--- sql/blog.sql
+++ sql/blog.sql
@@ -10,4 +10,35 @@ SET NAMES utf8mb4;
 DROP DATABASE IF EXISTS `blog`;
 CREATE DATABASE `blog` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
--- 2022-06-03 04:26:58
+USE `blog`;
+
+DROP TABLE IF EXISTS `blog_tag`;
+CREATE TABLE `blog_tag` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '标签名称',
+  `created_at` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `created_by` varchar(100) NOT NULL DEFAULT '' COMMENT '创建人',
+  `modified_at` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '修改时间',
+  `modified_by` varchar(100) NOT NULL DEFAULT '' COMMENT '修改人',
+  `deleted_at` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT '是否已删除 0 为未删除、1 为已删除'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `blog_article`;
+CREATE TABLE `blog_article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '''''' COMMENT '文章标题',
+  `desc` varchar(255) NOT NULL DEFAULT '''''' COMMENT '文章简述',
+  `cover_image_url` varchar(255) NOT NULL DEFAULT '''''' COMMENT '封面图片地址',
+  `content` longtext NOT NULL COMMENT '文章内容',
+  `state` tinyint(3) unsigned NOT NULL DEFAULT 1 COMMENT '状态0为禁用，1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章管理';
+
+DROP TABLE IF EXISTS `blog_article_tag`;
+CREATE TABLE `blog_article_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL COMMENT '文章ID',
+  `tag_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '标签ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章标签关联';
